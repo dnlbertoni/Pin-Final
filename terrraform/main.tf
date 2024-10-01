46ad52bb697232814b2c6fe0863d8cd40441aa46
Emiliano Palacio <palacio.ea@gmail.com>
2024-10-01 00:27:48 -0300

    [ADD] install sofware EC2 instance

--- terrraform/main.tf
+++ terrraform/main.tf
@@ -2,29 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami           = "ami-005fc0f236362e99f"   # AMI de Ubuntu Server 22.04 en us-east-1
-  instance_type = "t2.micro"
-  key_name      = "pin"
+data "aws_key_pair" "existing" {
+  key_name = "devops"  # Cambia a tu key pair existente
+}
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update -y
-              sudo apt-get install -y awscli docker.io jq
-              curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-              chmod +x ./kubectl
-              sudo mv ./kubectl /usr/local/bin/kubectl
-              curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-              EOF
-              
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-005fc0f236362e99f"  # AMI de Ubuntu Server 22.04 en us-east-1
+  instance_type = "t2.micro"
+  key_name      = data.aws_key_pair.existing.key_name
+
+  user_data = file("../scripts/initialInstance.sh")  # Cambia a la ruta correcta del script
+
   tags = {
     Name = "DevOps-Project"
   }
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "devopsPin"
-  public_key = file("/media/Containers/proyectos/devopsPin.pem")
 }
 
 output "instance_ip" {
